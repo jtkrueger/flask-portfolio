010f182efc0185f87d071f02d5fada899218092b
jkrueger <jordantkrueger@gmail.com>
2015-07-27 23:07:57 -0400

    In the midst of big changes. Installed Redis, made requirements.txt, taking slow steps to make quick admin routing and templates

--- routes.py
+++ routes.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+import redis
  
 app = Flask(__name__)      
- 
+
+# connect to redis data store
+r = redis.StrictRedis(host='localhost',port=6379,db=0)
+
 @app.route('/')
 def index():
+	# r.get()
 	return render_template('index.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+	if request.method == 'GET':
+		return render_template('create-sample.html')
+	elif request.method == 'POST':
+		title = request.form['title']
+		role = request.form['role']
+		date = request.form['date']
+
+		r.set(title +':role',role)
+		r.set(title +':date',date)
+
+
+		return render_template('created-sample.html', title=title, role=role, date=date)
  
 if __name__ == '__main__':
 	app.run(debug=True)
