1d49acce979175bb02d508fd861e9ecaa1361d35
Jordan Krueger <jordantkrueger@gmail.com>
2015-07-31 17:00:25 -0400

    Add jQuery and started scripts. For now, just trying to implement ajax calls for work sample templates. For now, postponing Redis database calls.

--- routes.py
+++ routes.py
@@ -4,12 +4,16 @@ import redis
 app = Flask(__name__)      
 
 # connect to redis data store
-r = redis.StrictRedis(host='localhost',port=6379,db=0)
+#r = redis.StrictRedis(host='localhost',port=6379,db=0)
 
 @app.route('/')
 def index():
 	# r.get()
 	return render_template('index.html')
+
+@app.route('/samples/accounting-degree')
+def accountingDegree():
+	return render_template('samples/accounting-degree.html')
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
@@ -20,8 +24,8 @@ def admin():
 		role = request.form['role']
 		date = request.form['date']
 
-		r.set(title +':role',role)
-		r.set(title +':date',date)
+		#r.set(title +':role',role)
+		#r.set(title +':date',date)
 
 
 		return render_template('created-sample.html', title=title, role=role, date=date)
