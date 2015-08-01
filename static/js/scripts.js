686e6efd4baed388115b20a6d44ce3bc87c83fc7
jkrueger <jordantkrueger@gmail.com>
2015-08-01 00:28:26 -0400

    More jquery and html setup.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-    $('.trigger').click(function(){
+    $('.trigger').on('click', function(e){
+    	e.preventDefault();
         $('#sample').load('/samples/accounting-degree');
     });
+
+    $('.close-button').on('click', function(e){
+    	e.preventDefault();
+    	$('#sample-content').hide();
+    });
+
+    $('#clicker').on('click', function(){
+    	alert('clicker');
+    	$('#clicker').hide();
+    	
+    });
+/*
+    $('.close').click(function(){
+    	alert("test");
+    })
+
+    $(.'introcontent').click(function(){
+    	alert('test');
+    })*/
 
 })
