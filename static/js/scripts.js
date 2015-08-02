8220385e325ca9423ec2f3b28904bd8fb201d275
Jordan Krueger <jordantkrueger@gmail.com>
2015-08-02 10:09:19 -0400

    Got scroll position to stay the same on sample modals. Worked a bit on modal styling.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
     $('.trigger').on('click', function(e){
     	e.preventDefault();
+        var scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+      ];
+      var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+      html.data('scroll-position', scrollPosition);
+      html.data('previous-overflow', html.css('overflow'));
+      html.css('overflow', 'hidden');
+      window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
         $('#sample').load('/samples/accounting-degree');
+
     });
 
     $('.close-button').on('click', function(e){
     	e.preventDefault();
-    	$('#sample-content').hide();
+        var html = jQuery('html');
+        html.css('overflow', 'auto');
+    	$('#sample-window').hide();
     });
 
     $('#clicker').on('click', function(){
@@ -14,6 +27,9 @@ $(document).ready(function(){
     	$('#clicker').hide();
     	
     });
+
+
+
 /*
     $('.close').click(function(){
     	alert("test");
