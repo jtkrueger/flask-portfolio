cf0dbc195dd9eeec266b8c7d91a39a9839d3d37a
Jordan Krueger <jordantkrueger@gmail.com>
2015-08-03 18:02:24 -0400

    Made target to close modal all surrounding space outside of modal.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,17 +2,16 @@ $(document).ready(function(){
     $('.trigger').on('click', function(e){
     	e.preventDefault();
         var scrollPosition = [
-        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-      ];
-      var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-      html.data('scroll-position', scrollPosition);
-      html.data('previous-overflow', html.css('overflow'));
-      html.css('overflow', 'hidden');
-      window.scrollTo(scrollPosition[0], scrollPosition[1]);
+            self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+        var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
 
         $('#sample').load('/samples/accounting-degree');
-
     });
 
     $('.close-button').on('click', function(e){
@@ -39,4 +38,15 @@ $(document).ready(function(){
     	alert('test');
     })*/
 
-})
+});
+
+$(document).mouseup(function (e)
+{
+    var container = $('.sample-content');
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $('#sample-window').hide();
+    }
+});
