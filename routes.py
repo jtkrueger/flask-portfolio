7890674deaca817b2d7d151c35ae7aac35b4ceeb
jkrueger <jordantkrueger@gmail.com>
2015-09-13 03:03:11 -0400

    Added URLs to samples for graceful degradation (disabled js). Also have sample templates extend layout for graceful degradation. Reorganized routes to follow portfolio order.

--- routes.py
+++ routes.py
@@ -13,14 +13,6 @@ def index():
 	# r.get()
 	return render_template('index.html')
 
-@app.route('/samples/accounting-degree')
-def accounting_degree():
-	return render_template('samples/accounting-degree.html')
-
-@app.route('/samples/anvil')
-def anvil():
-	return render_template('samples/anvil.html')
-
 @app.route('/samples/hay-merchant')
 def hay_merchant():
 	return render_template('samples/hay-merchant.html')
@@ -29,29 +21,37 @@ def hay_merchant():
 def nightingale():
 	return render_template('samples/nightingale.html')
 
-@app.route('/samples/julep')
-def julep():
-	return render_template('samples/julep.html')
-
 @app.route('/samples/underbelly')
 def underbelly():
 	return render_template('samples/underbelly.html')
-
-@app.route('/samples/hpc')
-def hpc():
-	return render_template('samples/hpc.html')
 
 @app.route('/samples/rice')
 def rice():
 	return render_template('samples/rice.html')
 
+@app.route('/samples/anvil')
+def anvil():
+	return render_template('samples/anvil.html')
+
+@app.route('/samples/julep')
+def julep():
+	return render_template('samples/julep.html')
+
 @app.route('/samples/pastry-war')
 def pastry_war():
 	return render_template('samples/pastry-war.html')
 
+@app.route('/samples/hpc')
+def hpc():
+	return render_template('samples/hpc.html')
+
 @app.route('/samples/icon-color')
 def icon_color():
 	return render_template('samples/icon-color.html')
+
+@app.route('/samples/accounting-degree')
+def accounting_degree():
+	return render_template('samples/accounting-degree.html')
 
 @app.route('/samples/job-board')
 def job_board():
